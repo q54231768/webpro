aa046c71d6b7345f1036ea5eacf59651985773a0
3117004935+刘浩航 <304074250@qq.com>
2020-06-27 22:45:45 +0800

    v3.0.3 sql文件编码有些问题改了下

--- 数据库/webpro.sql
+++ 数据库/webpro.sql
@@ -7,7 +7,7 @@
   password char(32) NOT NULL COMMENT '用户密码',
   delete_time bigint(20) NOT NULL COMMENT '用户删除时间,0代表用户账号有效,大于0代表用户失效,数值代表用户失效的时间需要转为时间戳',
   create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '用户注册时间',
-  last_login_time timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '用户上次登录时间',
+  last_login_time timestamp NOT NULL COMMENT '用户上次登录时间',
   landing_times int(10) NOT NULL COMMENT '用用户登录次数',
   PRIMARY KEY (user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';    
